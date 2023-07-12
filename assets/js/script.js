0a5dd5243191c4db2474c2eb6b4212b74396161e
Ross F <rfahan@gmail.com>
2023-07-12 16:56:37 -0600

    added pseudo code for js

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+//global variables
+// current date variable
+//request urls for apis
+
+//request urls for apis
+
+//html (query) selectors
+//variable for search
+//vairable for search input field
+//var searchButtonEl = document.querySelector('#id')
+
+//functions
+
+// search 
+function getCoordinates(city){
+    // add the city to the url as a query parameter
+
+    // make a fetch request to first api
+    // make a fetch request to the first api
+    // . then
+    // convert respone to json
+    //.then
+    //extract coordinates and save them as variable
+    // call getWeather
+}
+
+function getWeather(lat, long){   
+//add the coordinates to the url as a query parameter
+
+// make a fetch request to first api
+// make a fetch request to the first api
+// . then
+// convert respone to json
+//.then
+//extract 5 day forecast and current day forcast
+}
+
+function displayWeather(weatherData){
+//make weather data appear (text content append etc)
+}
+
+function loadSearchHistory(){
+    // find searched cities in localStorage
+    // display them
+    //creating button adding to page
+}
+
+//event listener for submit form and button input (submit button for form)
+//event listener for previous search buttons
+
+//some different options
+
+var formEl = document.querySelector("#form")
+
+formEl.addEventListener("submit", function() {
+    //do something
+})
+
+function formHandler(){
+//do some stuff what city was typed call get coordinates
+
+}
+
+formEl.addEventListener("submit", formHandler)
