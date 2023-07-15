986ca1d43727277b2a56889cf9e30824cc0c6f9c
Ross F <rfahan@gmail.com>
2023-07-14 22:17:51 -0600

    added 5 day forecase

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,7 @@ function displayWeather(weatherData) {
     // Clear previous weather data
     currentWeatherContainer.innerHTML = '';
     forecastContainer.innerHTML = '';
-  
+
     // Variables to store current weather data
     var currentWeather = weatherData.list[0];
     var cityName = weatherData.city.name;
@@ -71,33 +71,97 @@ function displayWeather(weatherData) {
     var windSpeed = currentWeather.wind.speed;
     var humidity = currentWeather.main.humidity;
     var date = new Date(currentWeather.dt * 1000).toLocaleDateString();
-  
+
     console.log(cityName);
-  
-    // Create HTML elements
-    var heading = document.createElement('h3');
-    var weatherIconImg = document.createElement('img');
-    var temperatureSpan = document.createElement('span');
-    var windSpeedSpan = document.createElement('span');
-    var humiditySpan = document.createElement('span');
-  
-    // Set inner HTML or text content
-    weatherIconImg.src = 'https://openweathermap.org/img/w/' + weatherIcon + '.png';
-    weatherIconImg.alt = 'Weather Icon';
-    heading.textContent = cityName + " " + "(" + date + ")" + " ";
-    temperatureSpan.textContent = 'Temperature: ' + temperature + '°';
-    windSpeedSpan.textContent = 'Wind Speed: ' + windSpeed + ' MPH';
-    humiditySpan.textContent = 'Humidity: ' + humidity + '%';
-  
-    // Append elements to currentWeatherContainer
-    heading.appendChild(weatherIconImg);
-    currentWeatherContainer.appendChild(heading);
-    currentWeatherContainer.appendChild(temperatureSpan);
-    currentWeatherContainer.appendChild(windSpeedSpan);
-    currentWeatherContainer.appendChild(humiditySpan);
-  
-    // Process and display 5-day forecast
-  }
+
+     // Create HTML elements
+     var heading = document.createElement('h3');
+     var weatherIconImg = document.createElement('img');
+     var temperatureSpan = document.createElement('span');
+     var windSpeedSpan = document.createElement('span');
+     var humiditySpan = document.createElement('span');
+ 
+     // Set inner HTML or text content
+     weatherIconImg.src = 'https://openweathermap.org/img/w/' + weatherIcon + '.png';
+     weatherIconImg.alt = 'Weather Icon';
+     heading.textContent = cityName + " " + "(" + date + ")" + " ";
+ 
+     // Create div elements to wrap spans
+     var temperatureDiv = document.createElement('div');
+     var windSpeedDiv = document.createElement('div');
+     var humidityDiv = document.createElement('div');
+ 
+     temperatureSpan.textContent = 'Temperature: ' + temperature + '°';
+     windSpeedSpan.textContent = 'Wind Speed: ' + windSpeed + ' MPH';
+     humiditySpan.textContent = 'Humidity: ' + humidity + '%';
+ 
+     // Append spans to respective div elements
+     temperatureDiv.appendChild(temperatureSpan);
+     windSpeedDiv.appendChild(windSpeedSpan);
+     humidityDiv.appendChild(humiditySpan);
+ 
+     // Append elements to currentWeatherContainer
+     heading.appendChild(weatherIconImg);
+     currentWeatherContainer.appendChild(heading);
+     currentWeatherContainer.appendChild(temperatureDiv);
+     currentWeatherContainer.appendChild(windSpeedDiv);
+     currentWeatherContainer.appendChild(humidityDiv);
+ 
+     // Process and display 5-day forecast
+     var forecastList = weatherData.list;
+ 
+     var forecastRow = document.createElement('div');
+     forecastRow.classList.add('row');
+
+    // Looping for 5 days (API provides data every 3 hours)
+    for (var i = 7; i < 47; i += 8) {
+        var forecast = forecastList[i];
+        var forecastCard = document.createElement('div');
+        forecastCard.classList.add('card', 'col-md-2');
+
+        var forecastDate = new Date(forecast.dt * 1000).toLocaleDateString();
+        var forecastIcon = forecast.weather[0].icon;
+        var forecastTemperature = forecast.main.temp;
+        var windSpeed = forecast.wind.speed;
+        var forecastHumidity = forecast.main.humidity;
+
+        var cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        var forecastDateHeading = document.createElement('h5');
+        forecastDateHeading.textContent = forecastDate;
+
+        var forecastIconImg = document.createElement('img');
+        forecastIconImg.src = 'https://openweathermap.org/img/w/' + forecastIcon + '.png';
+        forecastIconImg.alt = 'Weather Icon';
+
+        var forecastTemperatureDiv = document.createElement('div'); // Wrap forecastTemperatureSpan in a div
+        var windSpeedDiv = document.createElement('div'); // Wrap windSpeedSpan in a div
+        var forecastHumidityDiv = document.createElement('div'); // Wrap forecastHumiditySpan in a div
+        var forecastTemperatureSpan = document.createElement('span');
+        var windSpeedSpan = document.createElement('span');
+        var forecastHumiditySpan = document.createElement('span');
+
+        forecastTemperatureSpan.textContent = 'Temperature: ' + forecastTemperature + '°';
+        windSpeedSpan.textContent = 'Wind Speed: ' + windSpeed + ' MPH';
+        forecastHumiditySpan.textContent = 'Humidity: ' + forecastHumidity + '%';
+
+        forecastTemperatureDiv.appendChild(forecastTemperatureSpan);
+        windSpeedDiv.appendChild(windSpeedSpan);
+        forecastHumidityDiv.appendChild(forecastHumiditySpan);
+
+        cardBody.appendChild(forecastDateHeading);
+        cardBody.appendChild(forecastIconImg);
+        cardBody.appendChild(forecastTemperatureDiv);
+        cardBody.appendChild(windSpeedDiv);
+        cardBody.appendChild(forecastHumidityDiv);
+
+        forecastCard.appendChild(cardBody);
+        forecastRow.appendChild(forecastCard);
+    }
+
+    forecastContainer.appendChild(forecastRow);
+}
 
 function loadSearchHistory() {
     // find searched cities in localStorage
@@ -117,3 +181,4 @@ searchForm.addEventListener('submit', function (event) {
 
 
 //event listener for previous search buttons
+
